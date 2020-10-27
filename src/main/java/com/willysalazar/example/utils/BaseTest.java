eebaea47bbc6abee89111376ee4253feecec5d71
wsalazar <wsalazar@avenuecode.com>
2020-10-27 18:47:49 -0300

    :white_check_mark: refactory code tests

--- src/main/java/com/willysalazar/example/utils/BaseTest.java
+++ src/main/java/com/willysalazar/example/utils/BaseTest.java
@@ -3,6 +3,7 @@ package com.willysalazar.example.utils;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
 import com.willysalazar.example.driver.DriverFactory;
 import com.willysalazar.example.page.AbPage;
+import com.willysalazar.example.page.AddRemoveElementsPage;
 import com.willysalazar.example.page.WelcomePage;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
@@ -18,6 +19,7 @@ public abstract class BaseTest {
     protected static WebDriver driver;
     protected WelcomePage welcomePage;
     protected AbPage abPage;
+    protected AddRemoveElementsPage addRemoveElementsPage;
 
     public static WebDriver getDriver() {
         return driver;
@@ -30,6 +32,7 @@ public abstract class BaseTest {
         driver.get(returnConfigValue("url.base"));
         welcomePage = new WelcomePage(driver);
         abPage = new AbPage(driver);
+        addRemoveElementsPage = new AddRemoveElementsPage(driver);
     }
 
     @AfterMethod
