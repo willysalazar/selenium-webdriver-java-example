e8d6a8a2dace78893c753ea423107a099d04c1ca
wsalazar <wsalazar@avenuecode.com>
2020-10-28 18:59:52 -0300

    :recycle: add new waitElement

--- src/main/java/com/willysalazar/example/selenium/WaitElement.java
+++ src/main/java/com/willysalazar/example/selenium/WaitElement.java
@@ -8,7 +8,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class WaitElement {
 
     public static WebElement waitForMessage(WebDriver driver, WebElement webElement) {
-        int timeWait = 10;
+        int timeWait = 5;
+        WebDriverWait explicitWaitByElement = new WebDriverWait(driver, timeWait);
+        explicitWaitByElement.until(ExpectedConditions.visibilityOf(webElement));
+        return webElement;
+    }
+
+    public static WebElement waitVisibilityOf(WebDriver driver, WebElement webElement) {
+        int timeWait = 5;
         WebDriverWait explicitWaitByElement = new WebDriverWait(driver, timeWait);
         explicitWaitByElement.until(ExpectedConditions.visibilityOf(webElement));
         return webElement;
